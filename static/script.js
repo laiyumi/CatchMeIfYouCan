9fa3e69061e9b7c46415e8058d7a458ffe9b03f4
Yu Lai <yulai.linda@gmail.com>
2024-03-31 01:13:43 -0400

    updated

--- static/script.js
+++ static/script.js
@@ -1,9 +1,9 @@
+window.saveDataAcrossSessions = false;
+
 let context;
 let canvas;
 let targetPoint;
 let eyePoint;
-
-
 
 // set up the canvas
 canvas = document.getElementById('canvas');
@@ -15,31 +15,48 @@ var window_height = window.innerHeight;
 canvas.width = window_width;
 canvas.height = window_height;
 
-
+// call after the whole page is loaded
 window.onload = async function() {
-
     // start the webgazer tracker
+    let bufferX = [];
+    let bufferY = [];
+    let bufferSize = 4; // Size of the moving window for the moving average, adjust as needed
+
     await webgazer.setGazeListener(function(data, elapsedTime) {
-        if (data == null) {
+        if (data === null) {
             console.log("fail to track the eye");
             return;
         }
-        var xprediction = data.x; //these x coordinates are relative to the viewport
-        var yprediction = data.y; //these y coordinates are relative to the viewport
-        console.log(elapsedTime); //elapsed time is based on time since begin was called
-        console.log("eye prediction = " + xprediction + " " + yprediction);
+        
+        let xprediction = data.x; 
+        let yprediction = data.y; 
 
-        // update eye point coordinate based on the webgazer prediction
-        eyePoint.update(xprediction, yprediction);
+        // Update the moving averages.
+        bufferX.push(xprediction);
+        bufferY.push(yprediction);
+        if(bufferX.length > bufferSize) bufferX.shift(); // Remove the oldest value
+        if(bufferY.length > bufferSize) bufferY.shift(); // Remove the oldest value
+
+        // Compute smoothed predictions
+        let xSmoothed = bufferX.reduce((a, b) => a + b, 0) / bufferX.length;
+        let ySmoothed = bufferY.reduce((a, b) => a + b, 0) / bufferY.length;
+
+        // Update the eye coordinates with remediated data
+        eyePoint.update(xSmoothed, ySmoothed);
+
     }).begin();
 
-    webgazer.showVideoPreview(true); //shows all video canvsases
-    webgazer.showPredictionPoints(true); //shows a square every 100 milliseconds where current prediction is
+    // webgazer.setRegression("linear"); 
+    webgazer.setTracker("facedetection");
 
+    webgazer.showVideoPreview(true);
+    webgazer.showPredictionPoints(true);
 
-
+    console.log(webgazer.getRegression()) // Confirm current regression model
+    console.log(webgazer.getTracker()) // Confirm current tracker model
 
 };
+
 
 window.addEventListener('keypress', (event) => {
     if (event.key === 'e') {
@@ -53,19 +70,18 @@ window.addEventListener('keypress', (event) => {
 
 class Circle{
     constructor(x, y, radius, color){
-        this.x = canvas.width / 2;
-        this.y = canvas.height / 2;
-        // this.direction = "vertical";
+        this.x = x;
+        this.y = y;
         this.radius = radius;
         this.color = color;
         this.dx = 2;
         this.dy = 2;
     }
 
-    draw(){
+    draw() {
         context.beginPath();
-        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI); 
-        context.fillStyle = this.color;
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.fillStyle = `rgb(${this.color.r}, ${this.color.g}, ${this.color.b})`;
         context.fill();
     }
 
@@ -111,53 +127,49 @@ class Circle{
                 }
                 break;
         }
-
-        // if(direction == "horizontal"){
-        //     if(this.x + this.radius >= canvas.width || this.x - this.radius < 0){
-        //         this.dx = -this.dx;
-        //     }
-        //     if(this.x - this.radius <= 0){
-        //         // this.direction = "diagonal";
-        //         this.reset();
-        //     }
-        //     else{
-        //         this.x += this.dx;
-        //     }
-        //  }
-
-        // if(direction == "diagonal"){ //does not work yet.....
-        //     if (this.x + this.radius >= canvas.width || this.x - this.radius <= 0) {
-        //         this.dx = -this.dx;
-        //     }
-        //     else if (this.y + this.radius >= canvas.height || this.y - this.radius <= 0) {
-        //         this.dy = -this.dy;
-        //     }
-        
-        //     else if (this.x - this.radius <= 0 && this.y - this.radius <= 0) {
-        //         this.dx = -this.dx;
-        //         this.dy = -this.dy;
-        //     }
-        
-        //     else if (this.x + this.radius >= canvas.width && this.y - this.radius <= 0) {
-        //         this.dx = -this.dx;
-        //         this.dy = -this.dy;
-        //     }
-        
-        //     else if (this.x - this.radius <= 0 && this.y + this.radius >= canvas.height) {
-        //         this.dx = -this.dx;
-        //         this.dy = -this.dy;
-        //     }
-        
-        //     else if (this.x + this.radius >= canvas.width && this.y + this.radius >= canvas.height) {
-        //         this.dx = -this.dx;
-        //         this.dy = -this.dy;
-        //     }
-        // }
-
         this.draw();
         
     }
 }
+
+class Oval {
+    constructor(context, x, y, radiusX, radiusY, rotation, startAngle, endAngle) {
+        this.context = context;
+        this.x = x;
+        this.y = y;
+        this.radiusX = radiusX;
+        this.radiusY = radiusY;
+        this.rotation = rotation;
+        this.startAngle = startAngle;
+        this.endAngle = endAngle;
+    }
+
+    draw() {
+        this.context.beginPath();
+        this.context.ellipse(
+            this.x, this.y, this.radiusX, this.radiusY, this.rotation,
+            this.startAngle, this.endAngle
+        );
+        this.context.stroke();
+    }
+
+    compress(axis) {
+        switch(axis){
+            case "vertical":
+                if(this.radiusX > 0){
+                    this.radiusX -= 1;
+                }
+                break;
+            case "horizontal":
+                if(this.radiusY > 0){
+                    this.radiusY -= 1;
+                }
+                break;
+        }
+        this.draw();
+    }
+}
+
 
 class Area {
     constructor(x, y, width, height, color) {
@@ -186,48 +198,42 @@ class Area {
 // set the start point
 var middleX = window.innerWidth / 2;
 var middleY = window.innerHeight / 2;
-targetPoint = new Circle(middleX, middleY, 10, 'black');
+
+targetPoint = new Circle(middleX, middleY, 10, 'red');
+eyePoint = new Circle(300, 300, 30, 'yellow');
+targetPoint.draw();
+eyePoint.draw();
+
+var testOval = new Oval(context, 200, 200, 100, 50, 0, 0, 2 * Math.PI);
+
+
+
 
 // Create the animate function
 function animate() {
-    context.fillStyle = 'rgba(255, 255, 255, 1)';
+    // clear the canvas before each frame
+    context.fillStyle = 'rgba(255, 255, 255)';
     context.fillRect(0, 0, window.innerWidth, window.innerHeight);
 
-
+    // mark the valid area
     leftArea.draw();
     rightArea.draw();
     topArea.draw();
     bottomArea.draw();
 
-    targetPoint.move("right");
-    eyePoint.draw();
-
-    console.log("ball position = " + targetPoint.x + " " + targetPoint.y);
-    console.log("eye position = " + eyePoint.x + " " + eyePoint.y);
+    targetPoint.move("left");
+    // eyePoint.draw();
+    // testOval.compress("horizontal");
 
 
+
+    // console.log("ball position = " + targetPoint.x + " " + targetPoint.y);
+    // console.log("eye position = " + eyePoint.x + " " + eyePoint.y);
 
     requestAnimationFrame(animate);
+
+
 }
-
-
-
-function checkIfEyeInArea(eyePoint, area){
-    let coordinates = area.getCoordinates();
-    if(eyePoint.x >= coordinates.topLeft.x && eyePoint.x <= coordinates.topRight.x && eyePoint.y >= coordinates.topLeft.y && eyePoint.y <= coordinates.bottomLeft.y){
-        return true;
-    }
-    return false;
-}
-
-var countdownElement = document.getElementById("countdown");
-function startCountdown() {
-    var countdown = setInterval(function() {
-        console.log(count);
-        count--;
-    }, 1000);
-}
-
 
 
 
@@ -252,21 +258,36 @@ var bottomArea = new Area(2*cellWidth, 3*cellHeight, cellWidth, 2*cellHeight, 'y
 
 
 
-eyePoint = new Circle(400, 400, 30, 'yellow');
-targetPoint.draw();
-eyePoint.draw();
 
 var count = 5;
-// Check if the eye point is in right area
-while(checkIfEyeInArea(eyePoint, rightArea)){
-    // start counting down for 5 seconds
-    startCountdown();
-    
+var countdownElement = document.getElementById("countdown");
+
+// check if the circle is in the target area
+var checkIfInArea = function(targetPoint,leftArea){
+    var count = 5;
+
+    var leftAreaCoordinates = leftArea.getCoordinates();
+    if(targetPoint.x <= leftAreaCoordinates.topRight.x && targetPoint.y <= leftAreaCoordinates.topRight.y && targetPoint.y >= leftAreaCoordinates.bottomRight.y){
+            return true;
+        }
+    return false;
 }
-if(count === 0){
-    clearInterval(countdown);
-    console.log("Good Job!");
+
+// when the circle is in the target area, start the countdown
+while(checkIfInArea(targetPoint, leftArea)){
+    countdown();
 }
+
+function countdown(){
+    if(count >= 0){
+        count--;
+    } else {
+        console.log("Good Job!!");
+        // reset the circle to the middle
+    }
+}
+
+
 
 
 // Call the animate function
